6040872f1554954281ce3984321891fc53fe1881
lucas <lcs_demais@hotmail.com>
2022-12-10 07:06:24 -0300

    att

---
+++ src/Fundamentos/Livro/TesteFor.java
@@ -0,0 +1,20 @@
+package Fundamentos.Livro;
+
+import javax.swing.*;
+
+public class TesteFor {
+    public static void main(String[] args) {
+
+        float numero,media,soma;
+        int cont;
+        cont = 0;
+        soma = 0f;
+
+        for (cont = 0; cont < 3; cont++) {
+            numero = Float.parseFloat(JOptionPane.showInputDialog("Digite o número"));
+            soma = soma + numero;
+        }
+    media = soma / cont;
+        JOptionPane.showMessageDialog(null,"A média é : " + media);
+    }
+}
